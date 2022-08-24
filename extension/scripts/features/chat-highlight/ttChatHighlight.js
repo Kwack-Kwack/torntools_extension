7d68784af3159fb8ec82f4aa8c72f7555b35feec
Blair <87805559+itsBlair-hide@users.noreply.github.com>
2022-08-25 01:37:01 +0800

    Chat fixes for punctuation, highlight all usernames

--- extension/scripts/features/chat-highlight/ttChatHighlight.js
+++ extension/scripts/features/chat-highlight/ttChatHighlight.js
@@ -55,10 +55,15 @@
 	}
 
 	function applyHighlights(message) {
-		if (!message) return;
+		if (!message) {
+			console.log("!message");
+			return;
+		}
 		if (!message.find) console.log("DKK highlightChat", { message });
-		if (!highlights.length) return;
-
+		if (!highlights.length) {
+			console.log("!highlights.length");
+			return;
+		}
 		const sender = simplify(message.find("a").textContent).slice(0, -1);
 		const words = message.find("span").textContent.split(" ").map(simplify);
 
@@ -66,14 +71,22 @@
 		if (senderHighlights.length) {
 			message.find("a").style.color = senderHighlights[0].senderColor;
 			message.find("a").classList.add("tt-highlight");
+		} else {
+			if(!message.find("a").getAttribute("title")) {
+				message.find("a").style.color = "rgb(255,175,255)";
+				message.find("a").classList.add("tt-highlight");
+			}
 		}
 
 		for (const { name, color } of highlights) {
-			if (!words.includes(name)) continue;
-
-			message.find("span").parentElement.style.backgroundColor = color;
-			message.find("span").classList.add("tt-highlight");
-			break;
+			// if (!words.includes(name)) continue;
+			for(var word of words) {
+				if(word.includes(name)) {
+					message.find("span").parentElement.style.backgroundColor = color;
+					message.find("span").classList.add("tt-highlight");
+					break;
+				}
+			}
 		}
 
 		function simplify(text) {

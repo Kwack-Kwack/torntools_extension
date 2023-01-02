146f3797e3b7e3b5a621dd0bee8c9d4511b61c86
Blair <87805559+Kwack-Kwack@users.noreply.github.com>
2023-01-02 19:48:34 +0800

    Add supported urls to prevent unexpected behaviours for copy ID

--- extension/scripts/features/copy-id/ttCopyID.js
+++ extension/scripts/features/copy-id/ttCopyID.js
@@ -1,11 +1,17 @@
 "use strict";
 
 chrome.runtime.onMessage.addListener((url) => {
-	const id = url.replace(/\D/g, "");
-	if (id) {
-		toClipboard(id);
+	const supportedPages = /factions.php|profiles.php|joblist.php/
+	if (!!supportedPages.test(url)) {
+		const id = url.replace(/\D+/g, ",").split(",").filter(e => e)[0] //TODO: let user choose IDs when multiple are present
+		if (id) {
+			toClipboard(id);
+		} else {
+			console.warn(`[TornTools] Copy ID - No ID found in url ${url}, clipboard cleared.`);
+			toClipboard("");
+		}
 	} else {
-		console.warn(`[TornTools] Copy ID - no ID found in url ${url}`);
+		console.warn(`[TornTools] Copy ID - Unsupported destination URL, clipboard cleared.`);
 		toClipboard("");
 	}
 });
